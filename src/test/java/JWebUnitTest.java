a6aca055bd5e52d4088a4a8f473e7039f16aa44a
codingvesna <222-2016@ftn.edu.rs>
2021-12-12 09:24:52 +0100

    setBaseUrl changed

--- src/test/java/JWebUnitTest.java
+++ src/test/java/JWebUnitTest.java
@@ -6,7 +6,8 @@ import static net.sourceforge.jwebunit.junit.JWebUnit.*;
 public class JWebUnitTest {
     @Before
     public void prepare() {
-        setBaseUrl("http://localhost:8081/webapp");
+       // setBaseUrl("http://localhost:8081/webapp");
+        setBaseUrl("http://webapp-env.eba-ptnmqh8m.eu-west-1.elasticbeanstalk.com/");
     }
     @Test
     public void testLoginPage() {
