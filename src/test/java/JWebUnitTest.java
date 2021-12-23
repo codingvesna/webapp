4ab297f1bf5d79f392e8a17f57a5124b1588a059
Vesna Milovanovic <75800171+codingvesna@users.noreply.github.com>
2021-12-23 09:07:07 +0100

    Update JWebUnitTest.java

--- src/test/java/JWebUnitTest.java
+++ src/test/java/JWebUnitTest.java
@@ -7,7 +7,7 @@ public class JWebUnitTest {
     @Before
     public void prepare() {
        // setBaseUrl("http://localhost:8081/webapp");
-        setBaseUrl("http://webapp-env.eba-ptnmqh8m.eu-west-1.elasticbeanstalk.com/");
+        setBaseUrl("http://java-web-app.eu-west-1.elasticbeanstalk.com/");
     }
     @Test
     public void testLoginPage() {
